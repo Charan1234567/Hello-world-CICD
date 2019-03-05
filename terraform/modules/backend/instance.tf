6a0c8d234575c38ce99f5e21431ac341406ee091
dineshkodavatikanti <46729238+dineshkodavatikanti@users.noreply.github.com>
2019-03-05 14:52:45 -0800

    Update instance.tf

--- terraform/modules/backend/instance.tf
+++ terraform/modules/backend/instance.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "app" {
 
   provisioner "local-exec" {
     #command = "sleep 0; ANSIBLE_HOST_KEY_CHECKING=False /usr/bin/ansible-playbook -u ${var.ssh_user} --private-key ~/.ssh/ansible.pem -i '${self.private_ip},' '../../ansible/site.yml'"
-    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False /usr/local/bin/ansible-playbook -u ${var.ssh_user} --private-key ~/.ssh/ansible.pem -i '${self.private_ip},' '../../ansible/site.yml'"
+    command = "sleep 120; ANSIBLE_HOST_KEY_CHECKING=False /usr/bin/ansible-playbook-2.7 -u ${var.ssh_user} --private-key ~/.ssh/ansible.pem -i '${self.private_ip},' '../../ansible/site.yml'"
     }
 
   tags {
